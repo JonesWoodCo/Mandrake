bc4fee3fb8a58d7ac38d1b018d1bcb0360da4104
Topher <cjones.wingsofgold@gmail.com>
2015-01-27 12:35:05 -0600

    test out of meta tags on index

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     
     <!--meta tags-->
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
      
       <!--font-->
      <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>

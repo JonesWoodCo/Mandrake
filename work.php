f07e9c834a00b3a48a9a670d18d36ee3fb48a558
Topher <cjones@sycamoreleaf.com>
2015-01-30 16:40:05 -0600

    asdfasd

--- work.php
+++ work.php
@@ -95,7 +95,7 @@
 		<div id="container" class="container">
                         <ul id="scene" class="scene border fill">
 			      <li class="layer expand-width backStars" data-depth="1.00"><img class="backStars" src="img/transparent.png"></li>
-			      <li class="layer expand-width" data-depth="0.20"><img class="logo" src='img/mandrakeLogo2.png'></li>
+			      <li class="layer expand-width" data-depth="0.20"></li>
                         </ul>
 		</div>
 		<div class="socialFooterLinks">

244aa0f174a307a2185ee16f0b8bb1afae57ea06
Topher <cjones@sycamoreleaf.com>
2015-02-25 07:49:04 -0600

    stat counter

--- index.php
+++ index.php
@@ -116,6 +116,23 @@
 			var parallax = new Parallax(scene);
 	
 		</script>
+				<!-- Start of StatCounter Code -->
+		<script type="text/javascript">
+			var sc_project=10312296; 
+			var sc_invisible=1; 
+			var sc_security="0b0e0010"; 
+			var scJsHost = (("https:" == document.location.protocol) ?
+			"https://secure." : "http://www.");
+			document.write("<sc"+"ript type='text/javascript' src='" +
+			scJsHost+
+			"statcounter.com/counter/counter.js'></"+"script>");
+		</script>
+		<noscript><div class="statcounter"><a title="web analytics"
+		href="http://statcounter.com/" target="_blank"><img
+		class="statcounter"
+		src="http://c.statcounter.com/10312296/0/0b0e0010/1/"
+		alt="web analytics"></a></div></noscript>
+		<!-- End of StatCounter Code -->
 	</body>
 </html>
 <!--mobile

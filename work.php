9b28993dd906dc8ceb03b3e7d4ff2f6fbeb82983
Topher <cjones@sycamoreleaf.com>
2015-02-27 17:17:39 -0600

    workmobile done

--- work.php
+++ work.php
@@ -34,10 +34,10 @@
 	  <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
 	  <link rel="stylesheet" type="text/css" href="styles/work.css"/>
 	  <link href="message.css" rel="stylesheet">
-	    
-	     <!--mobile and tablet styling -->
-	  <link rel="stylesheet" media="handheld, only screen and (max-device-width: 600px)" href="styles/mobile.css">
-	  <link rel="stylesheet" media="only screen and (min-width: 601px) and (max-width: 800px)" href="styles/tablet.css">
+            
+	    <!--mobile and tablet styling-->
+         <link rel="stylesheet" media="handheld, only screen and (max-device-width: 600px)" href="styles/workMobile.css">
+	 <link rel="stylesheet" media="only screen and (min-width: 601px) and (max-width: 800px)" href="styles/workTablet.css">
 	    
 	  <!-- JAVASCRIPT -->
 	  
@@ -94,10 +94,10 @@
   
 		<div id="container" class="container">
 		      <ul id="scene" class="scene border fill">
-			      <li class="layer expand-width backStars" data-depth="1.00"><img style="height: 500px;" class="backSky" src="img/transparent.png"></li>
+			      <li class="layer expand-width backStars" data-depth="1.00"><img class="backSky" src="img/transparent.png"></li>
 			      <li class="layer expand-width" data-depth="1.00"><!--possibly add another layer here later--></li>
-			      <li class="layer expand-width" data-depth="0.10"><img style="margin-top: 50px; margin-left: auto; margin-right: auto;" class="mountains" src="img/mountains2.png"></li>
-			      <li class="layer expand-width" data-depth="0.40"><img style="width: 100%; margin-top: 30px;" class="clouds-forefront" src="img/workclouds.png"></li>
+			      <li class="layer expand-width" data-depth="0.10"><img class="mountains" src="img/mountains2.png"></li>
+			      <li class="layer expand-width" data-depth="0.40"><img class="clouds-forefront" src="img/workclouds.png"></li>
 			      <li class="layer expand-width" data-depth="0.0"></li>
 		      </ul>
 		</div>
